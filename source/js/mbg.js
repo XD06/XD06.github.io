2c90107eacdb943ada8fb0541afe1f7d82a696ba
XDo6 <2383032306@qq.com>
2025-02-27 11:29:01 +0800

    updata

--- source/js/mbg.js
+++ source/js/mbg.js
@@ -3,6 +3,27 @@
 function saveData(name, data) {
     localStorage.setItem(name, JSON.stringify({ 'time': Date.now(), 'data': data }))
 }
+let styleConfig = {
+    fontFamily: 'inherit'
+};
+
+// 初始化字体设置
+function initFont() {
+    const saved = loadData('fontConfig', 1440);
+    if (saved) {
+        styleConfig.fontFamily = saved;
+        document.documentElement.style.setProperty('--global-font', `'${saved}'`); // 添加引号
+    }
+}
+function updateFont(value) {
+    styleConfig.fontFamily = value;
+    applyFontStyle();
+}
+// 应用字体样式
+function applyFontStyle() {
+    document.documentElement.style.setProperty('--global-font', styleConfig.fontFamily);
+    saveData('fontConfig', styleConfig.fontFamily);
+}
 
 // 取数据
 // name：命名 time：过期时长,单位分钟,如传入30,即加载数据时如果超出30分钟返回0,否则返回数据
@@ -48,12 +69,12 @@ function createWinbox() {
     winbox = WinBox({
         id: 'changeBgBox',
         index: 999,
-        title: "切换背景",
+        title: "切换设置",
         x: "center",
         y: "center",
         minwidth: '300px',
         height: "60%",
-        background: '#49b1f5',
+        background: 'black',
         onmaximize: () => { div.innerHTML = `<style>body::-webkit-scrollbar {display: none;}div#changeBgBox {width: 100% !important;}</style>` },
         onrestore: () => { div.innerHTML = '' }
     });
@@ -63,30 +84,84 @@ function createWinbox() {
     // 每一类我放了一个演示，直接往下复制粘贴 a标签 就可以，需要注意的是 函数里面的链接 冒号前面需要添加反斜杠\进行转义
     winbox.body.innerHTML = `
     <div id="article-container" style="padding:10px;">
-    
+    <h2>文字样式</h2>
+    <div class="stylebox">
+      <div class="font-control">
+        <select id="fontSelector" onchange="updateFont(this.value)">
+          <option value="inherit">默认字体</option>
+          <option value="Hei">微软雅黑</option>
+          <option value="FangSong">仿宋</option>
+          <option value="Kaiti">楷体</option>
+          <option value="ZiHunBaiGeTianXing">字魂白鸽天行体</option>
+        </select>
+        </div>
+    </div>
     <p><button onclick="localStorage.removeItem('blogbg');location.reload();" style="background:#5fcdff;display:block;width:100%;padding: 15px 0;border-radius:6px;color:white;"><i class="fa-solid fa-arrows-rotate"></i> 点我恢复默认背景</button></p>
     <h2 id="图片（手机）"><a href="#图片（手机）" class="headerlink" title="图片（手机）"></a>图片（手机）</h2>
     <div class="bgbox">
-    <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://img.vm.laomishuo.com/image/2021/12/2021122715170589.jpeg)" class="pimgbox" onclick="changeBg('url(https\://img.vm.laomishuo.com/image/2021/12/2021122715170589.jpeg)')"></a>
+    <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4fd8d0e0a243d4065cd7.png)" class="pimgbox" onclick="changeBg('url(https\://pic1.imgdb.cn/item/67bf4fd8d0e0a243d4065cd7.png)')"></a>
+    <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4fd8d0e0a243d4065cd8.jpg)" class="pimgbox" onclick="changeBg('url(https\://pic1.imgdb.cn/item/67bf4fd8d0e0a243d4065cd8.jpg)')"></a>
+    <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4fd9d0e0a243d4065cd9.jpg)" class="pimgbox" onclick="changeBg('url(https\://pic1.imgdb.cn/item/67bf4fd9d0e0a243d4065cd9.jpg)')"></a>
+    <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4fd9d0e0a243d4065cda.png)" class="pimgbox" onclick="changeBg('url(https\://pic1.imgdb.cn/item/67bf4fd9d0e0a243d4065cda.png)')"></a>
+    <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4fd9d0e0a243d4065cdb.jpg)" class="pimgbox" onclick="changeBg('url(https\://pic1.imgdb.cn/item/67bf4fd9d0e0a243d4065cdb.jpg)')"></a>
+    <a href="javascript:;" rel="noopener external nofollow" 
+   style="background-image:url(https://dsk.dskblog.top/wopaper/【哲风壁纸】二次元美女-动漫美女.67xlezlyjf.webp)" 
+   class="pimgbox" 
+   onclick="changeBg('url(https\://dsk.dskblog.top/wopaper/【哲风壁纸】二次元美女-动漫美女.67xlezlyjf.webp)')">
+</a>
     </div>
     
     <h2 id="图片（电脑）"><a href="#图片（电脑）" class="headerlink" title="图片（电脑）"></a>图片（电脑）</h2>
     <div class="bgbox">
-    <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://cn.bing.com/th?id=OHR.GBRTurtle_ZH-CN6069093254_1920x1080.jpg)" class="imgbox" onclick="changeBg('url(https\://cn.bing.com/th?id=OHR.GBRTurtle_ZH-CN6069093254_1920x1080.jpg)')"></a>
+    <a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4f65d0e0a243d4065cbd.png)" class="imgbox" onclick="changeBg('url(https://pic1.imgdb.cn/item/67bf4f65d0e0a243d4065cbd.png)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4f65d0e0a243d4065cbc.jpg)" class="imgbox" onclick="changeBg('url(https://pic1.imgdb.cn/item/67bf4f65d0e0a243d4065cbc.jpg)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4f53d0e0a243d4065cb8.png)" class="imgbox" onclick="changeBg('url(https://pic1.imgdb.cn/item/67bf4f53d0e0a243d4065cb8.png)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4f53d0e0a243d4065cb7.jpg)" class="imgbox" onclick="changeBg('url(https://pic1.imgdb.cn/item/67bf4f53d0e0a243d4065cb7.jpg)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4f53d0e0a243d4065cb6.jpg)" class="imgbox" onclick="changeBg('url(https://pic1.imgdb.cn/item/67bf4f53d0e0a243d4065cb6.jpg)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4f52d0e0a243d4065cb5.jpg)" class="imgbox" onclick="changeBg('url(https://pic1.imgdb.cn/item/67bf4f52d0e0a243d4065cb5.jpg)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4f52d0e0a243d4065cb3.jpg)" class="imgbox" onclick="changeBg('url(https://pic1.imgdb.cn/item/67bf4f52d0e0a243d4065cb3.jpg)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4effd0e0a243d4065c9e.jpg)" class="imgbox" onclick="changeBg('url(https://pic1.imgdb.cn/item/67bf4effd0e0a243d4065c9e.jpg)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4effd0e0a243d4065c9d.jpg)" class="imgbox" onclick="changeBg('url(https://pic1.imgdb.cn/item/67bf4effd0e0a243d4065c9d.jpg)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4efed0e0a243d4065c9c.jpg)" class="imgbox" onclick="changeBg('url(https://pic1.imgdb.cn/item/67bf4efed0e0a243d4065c9c.jpg)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4efed0e0a243d4065c9b.png)" class="imgbox" onclick="changeBg('url(https://pic1.imgdb.cn/item/67bf4efed0e0a243d4065c9b.png)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67bf4efdd0e0a243d4065c9a.jpg)" class="imgbox" onclick="changeBg('url(https://pic1.imgdb.cn/item/67bf4efdd0e0a243d4065c9a.jpg)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" style="background-image:url(https://pic1.imgdb.cn/item/67b9d202d0e0a243d4022516.jpg)" class="imgbox" onclick="changeBg('url(https://pic1.imgdb.cn/item/67b9d202d0e0a243d4022516.jpg)')"></a>
+
     </div>
     
     
     
     <h2 id="渐变色"><a href="#渐变色" class="headerlink" title="渐变色"></a>渐变色</h2>
     <div class="bgbox">
-    <a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(to right, #eecda3, #ef629f)" onclick="changeBg('linear-gradient(to right, #eecda3, #ef629f)')"></a>
+    <a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(to right, #4facfe, #00f2fe)" onclick="changeBg('linear-gradient(to right, #4facfe, #00f2fe)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(to right, #ff758c, #ff7eb3)" onclick="changeBg('linear-gradient(to right, #ff758c, #ff7eb3)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(to right, #43e97b, #38f9d7)" onclick="changeBg('linear-gradient(to right, #43e97b, #38f9d7)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(to right, #9d50bb, #6e48aa)" onclick="changeBg('linear-gradient(to right, #9d50bb, #6e48aa)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(to right, #ff9a9e, #ff6b6b)" onclick="changeBg('linear-gradient(to right, #ff9a9e, #ff6b6b)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(45deg, #f6d365, #fda085)" onclick="changeBg('linear-gradient(45deg, #f6d365, #fda085)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(45deg, #00ff87, #60efff)" onclick="changeBg('linear-gradient(45deg, #00ff87, #60efff)')"></a>
+<a href="javascript:;" rel="noopener external nofollow" class="box" style="background: linear-gradient(45deg, #ff0844, #ffb199)" onclick="changeBg('linear-gradient(45deg, #ff0844, #ffb199)')"></a>
     </div>
     
     <h2 id="纯色"><a href="#纯色" class="headerlink" title="纯色"></a>纯色</h2>
     <div class="bgbox">
-    <a href="javascript:;" rel="noopener external nofollow" class="box" style="background: #7D9D9C" onclick="changeBg('#7D9D9C')"></a> 
-    </div>
-`;
+    <a href="javascript:;" rel="noopener external nofollow" class="box" style="background-color: #ff0000" onclick="changeBg('#ff0000')"></a>
+<a href="javascript:;" rel="noopener external nofollow" class="box" style="background-color: #0000ff" onclick="changeBg('#0000ff')"></a>
+<a href="javascript:;" rel="noopener external nofollow" class="box" style="background-color: #00ff00" onclick="changeBg('#00ff00')"></a>
+<a href="javascript:;" rel="noopener external nofollow" class="box" style="background-color: #ffff00" onclick="changeBg('#ffff00')"></a>
+<a href="javascript:;" rel="noopener external nofollow" class="box" style="background-color: #800080" onclick="changeBg('#800080')"></a>
+<a href="javascript:;" rel="noopener external nofollow" class="box" style="background-color: #ffa500" onclick="changeBg('#ffa500')"></a>
+<a href="javascript:;" rel="noopener external nofollow" class="box" style="background-color: #ff69b4" onclick="changeBg('#ff69b4')"></a>
+<a href="javascript:;" rel="noopener external nofollow" class="box" style="background-color: #00ffff" onclick="changeBg('#00ffff')"></a>
+   </div>
+   </div>
+  </div>`;
+    setTimeout(() => {
+        const fontSelector = document.getElementById('fontSelector');
+        if (fontSelector) {
+            fontSelector.value = styleConfig.fontFamily;
+        }
+    }, 100);
 }
 
 // 适应窗口大小
@@ -106,3 +181,20 @@ function toggleWinbox() {
     if (document.querySelector('#changeBgBox')) winbox.toggleClass('hide');
     else createWinbox();
 }
+// 在文件末尾添加以下代码
+document.addEventListener('DOMContentLoaded', function() {
+    // 初始化字体设置
+    initFont();
+    
+    // 绑定字体选择器（需要确保select元素有正确ID）
+    const fontSelector = document.getElementById('fontSelector');
+    if (fontSelector) {
+        fontSelector.value = styleConfig.fontFamily;
+        fontSelector.addEventListener('change', function() {
+            styleConfig.fontFamily = this.value;
+            applyFontStyle();
+        });
+    } else {
+        console.warn('未找到字体选择器元素');
+    }
+});
